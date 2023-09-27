37742c885f1987d2b221a24d0927f0fc34a4cbf8
neelakantaganesh23 <130562632+neelakantaganesh23@users.noreply.github.com>
2023-09-27 14:52:18 +0530

    Delete Hackathon/styling.py

--- Hackathon/styling.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def background_styling_for_login():
-    return """ <style>
-       body {
-        background: #003153;
-        font-family: 'Helvetica', 'Arial', sans-serif;
-    }
-     </style> """
